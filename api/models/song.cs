4465a2b2b5729b004cdb90997d90fda1c5358770
Will Tidwell <jwtidwell1@crimson.ua.edu>
2023-04-29 13:41:15 -0500

    Added files

---
+++ api/models/song.cs
@@ -0,0 +1,11 @@
+namespace willt.Desktop.School_Stuff.MIS_321.pa3spring
+{
+    public class song
+    {
+        public int songId {get; set;}
+        public string artistName {get; set;}
+        public string title {get; set;}
+        public bool isFavorited {get; set;}
+        public bool isDeleted {get; set;}
+    }
+}
